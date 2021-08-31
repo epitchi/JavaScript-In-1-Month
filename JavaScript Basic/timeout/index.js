df6a0d3cf3457536fd6161a2c0530414c9869699
epitchi <thienvanlea1@gmail.com>
2021-08-31 16:46:54 +0700

    set time out

---
+++ JavaScript Basic/timeout/index.js
@@ -0,0 +1,10 @@
+// setTimeout
+// clearTimeout
+//setTimeout(fn, ms) //milliseconds
+//timer
+console.log('Start');
+var timeoutId = setTimeout(function(){
+  console.log('Finish');
+}, 1000);
+console.log('Done');
+clearTimeout(timeoutId);
