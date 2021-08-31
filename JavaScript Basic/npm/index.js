9d0215192c7d7c4e6ca48540be4390791d659a55
epitchi <thienvanlea1@gmail.com>
2021-08-31 14:26:15 +0700

    Node package manager

---
+++ JavaScript Basic/npm/index.js
@@ -0,0 +1,19 @@
+// npm = node package manager
+
+var readlineSync = require('readline-sync');
+
+// var userName = readlineSync.question('May I have your name? ');
+// console.log('Hi ' + userName+ "!");
+// var languages = [];
+// var language = readlineSync.question('What is your moterh language? ');
+// languages.push(language);
+// console.log(languages);
+var pet = {};
+var name = readlineSync.question('Your pet name: ');
+var gender = readlineSync.question('Pet gender (Male/Female): ');
+var weight = readlineSync.question('Weight?: ');
+pet.name = name;
+pet.gender = gender;
+pet.weight = parseInt(weight);
+
+console.log(pet);
