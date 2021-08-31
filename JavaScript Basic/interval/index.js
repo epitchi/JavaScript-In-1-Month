d6e768c781ed0cc6dd644669e1dfbd3c88b8f5a3
epitchi <thienvanlea1@gmail.com>
2021-08-31 16:51:31 +0700

    interval

---
+++ JavaScript Basic/interval/index.js
@@ -0,0 +1,11 @@
+// setInterval
+// clearInterval
+// setInterval(fn, ms);
+var i = 0;
+var intervalId = setInterval(function(){
+  ++i;
+  console.log(i);
+  if(i === 10){
+    clearInterval(intervalId);
+  }
+}, 1000);
