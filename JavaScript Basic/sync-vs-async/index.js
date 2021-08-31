655ddf95e8f04b160c5475a3f842329135ac34df
epitchi <thienvanlea1@gmail.com>
2021-08-31 15:45:25 +0700

    Sync vs Async

---
+++ JavaScript Basic/sync-vs-async/index.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+// console.log('Start');
+// var song1 = fs.readFileSync('song1.txt', {encoding: 'utf8'});
+// console.log(song1);
+// var song2 = fs.readFileSync('song2.txt', {encoding: 'utf8'});
+// console.log(song2);
+// console.log('End');
+// Make coffee -> 5 minutes
+// Wear clothes
+// Making bed
+// Sync
+// |--------------------|-----------|------------|------>
+// Make coffee    Coffee done     Wear done     Done
+//                Wear clothes    Make bed
+// Async
+// |------------|-------------|-----------|-------->
+// Make coffee Wear clothes Make bed    Done
+console.log('Start');
+fs.readFile('song1.txt', {encoding: 'utf8'}, function(err, song1) {
+  console.log(song1);
+});
+console.log('End');
+
+// Read/Write to FS
+// Network 
